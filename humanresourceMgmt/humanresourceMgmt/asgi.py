4b7d262dbde70d6c8cf96c90ef7608a6a39ac2c2
DagTed <101938996+DagTed@users.noreply.github.com>
2023-03-30 18:16:10 +0300

    Add files via upload

---
+++ humanresourceMgmt/humanresourceMgmt/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for humanresourceMgmt project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'humanresourceMgmt.settings')
+
+application = get_asgi_application()
